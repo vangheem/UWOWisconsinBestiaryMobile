249580c4ee1e6c4b166228e41ee2061f9e7bb5de
Nathan Van Gheem <vangheem@gmail.com>
2013-08-08 02:30:30 -0700

    fix tablet

--- Resources/ui/tablet/ApplicationWindow.js
+++ Resources/ui/tablet/ApplicationWindow.js
@@ -1,46 +1,34 @@
 function ApplicationWindow() {
-	//declare module dependencies
-	var MasterView = require('ui/common/MasterView'),
-		DetailView = require('ui/common/DetailView');
-		
-	//create object instance
-	var self = Ti.UI.createWindow({
-		backgroundColor:'#ffffff'
-	});
-		
-	//construct UI
-	var masterView = new MasterView(),
-		detailView = new DetailView();
-		
-	masterView.borderColor = '#000';
-	masterView.borderWidth = 1;
-		
-	//create master view container
-	var masterContainer = Ti.UI.createView({
-		top:0,
-		bottom:0,
-		left:0,
-		width:240
-	});
-	masterContainer.add(masterView);
-	self.add(masterContainer);
-	
-	//create detail view container
-	var detailContainer = Ti.UI.createView({
-		top:0,
-		bottom:0,
-		right:0,
-		left:240
-	});
-	detailContainer.add(detailView);
-	self.add(detailContainer);
-	
-	//add behavior for master view
-	masterView.addEventListener('itemSelected', function(e) {
-		detailView.fireEvent('itemSelected',e);
-	});
-	
-	return self;
+  var self = this;
+  self.largeButtonHeight = 400;
+  self.buttonWidth = 500;
+  self.buttonHeight = 140;
+
+  //create object instance
+  self.win = Ti.UI.createWindow({
+    title:'Wisconsin Bestiary',
+    exitOnClose:true,
+    navBarHidden:false,
+    backgroundColor:'#ffffff',
+    orientationModes: [Ti.UI.PORTRAIT]
+  });
+
+  self.add = function(container){
+    self.win.add(container);
+  };
+
+  self.open = function(){
+    self.win.open();
+  };
+
+  self.openNew = function(win){
+    win.open();
+  };
+
+  var MainView = require('ui/common/MainView');
+  self.mainView = new MainView(self);
+
+  return self;
 }
 
 module.exports = ApplicationWindow;
